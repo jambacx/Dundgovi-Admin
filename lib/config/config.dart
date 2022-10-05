a213b791ae0b57f43f3f360df6009d1ed6ddffe3
JambaCX <ganzojamba@gmail.com>
2022-10-05 15:59:31 +0800

    publish: feat

---
+++ lib/config/config.dart
@@ -0,0 +1,9 @@
+
+class Config{
+  final String appName = 'Travel Hour'; //app name
+  final String testerPassword = 'tester1234';  //testing password - don't use this password in the database (you can change this testing password too)
+
+  //firebase server token for push notication
+  final String serverToken = 'AAAA1cgQa-M:APA91bFMD4KswezZfuap6nNBd24JDAIMLXIfni3fHKvd*****************';
+  final String icon = 'assets/images/icon.png'; // app icon
+}
