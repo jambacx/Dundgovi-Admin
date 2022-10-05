96a00321021d4f4a0540032747119759bc0cfd2f
JambaCX <ganzojamba@gmail.com>
2022-10-05 16:04:23 +0800

    fix: plate added

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,9 +1,10 @@
-
-class Config{
-  final String appName = 'Travel Hour'; //app name
-  final String testerPassword = 'tester1234';  //testing password - don't use this password in the database (you can change this testing password too)
+class Config {
+  final String appName = 'Dundgovi'; //app name
+  final String testerPassword =
+      'tester1234'; //testing password - don't use this password in the database (you can change this testing password too)
 
   //firebase server token for push notication
-  final String serverToken = 'AAAA1cgQa-M:APA91bFMD4KswezZfuap6nNBd24JDAIMLXIfni3fHKvd*****************';
+  final String serverToken =
+      'AAAAq30YB1c:APA91bEFInAhlLh9yz97CU0-2BD2_c5ZbfcNwGigm68WRmJTSSkTqKaa4ljP-tAy5y5ss8Fc50NbMUGs-RzQmST1uxnbxtLru-ZhFILNDulj3e85UVQC2G0rbgib0-3H_Rckk7X5IfSI';
   final String icon = 'assets/images/icon.png'; // app icon
 }
